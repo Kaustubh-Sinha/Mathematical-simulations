2871ecca9047bf3ad716cf9cb774d75d1a74e1a7
Kaustubh <59376003+Kaustubh-Sinha@users.noreply.github.com>
2021-06-10 10:33:37 -0400

    Prime lines

--- Legendres_conjecture.m
+++ Legendres_conjecture.m
@@ -1,16 +1,23 @@
-%Legendre's conjecture
+%--------------------------------------------------------------------------
+%This program prints the prime lines within Legendre's conjecture on which 
+%primes can exist. It can be observed to see the trend that some lines
+%extend to limited infinities which makes it possibly inclusive of primes
+%hence giving an inconclusive result to the conjecture.
 
+%Initialize the values of x:
 x = linspace(1,1000,1000);
+
+%Calculate y and y^2
 y = x.^2;
 y1 = (x+1).^2;
 
+%Plots line between the y and y1 points, which would have  primes on them
 for i = 1:length(x)
     plot([i,y(i)],[i,y1(i)])
     hold on
 end
 
-
-% plot(x,y)
-% hold on
-% plot(x,y1)
+%----------------------------------Le FIN----------------------------------
+%The author of the program makes this program available open-source under 
+%Creative Commons License.
 
